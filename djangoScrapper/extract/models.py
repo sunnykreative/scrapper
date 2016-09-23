5de7d2a50b34c8dc47d78269444556c74b22f494
sunnykreative <sunnykr78.kreative@gmail.com>
2016-09-23 18:18:13 +0530

    Scapper Changes done

--- djangoScrapper/extract/models.py
+++ djangoScrapper/extract/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Scrape(models.Model):	
+	import requests
+	r = requests.get("https://www.amazon.com/dp/b0042rum9g",headers={})
--- djangoScrapper/extract/views.py
+++ djangoScrapper/extract/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.template import RequestContext
+from .models import Scrape
+
 # Create your views here.
 def index(request):
 	return render(request,'home.html',{})
 
 def api(request):
-	return render(request,'api.html',{'request':request})
+
+	return render(request,'api.html',{'request':request,'data':Scrape})
 
 def contact(request):
 	return render(request,'contact.html',{})	
