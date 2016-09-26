71b803dd41d6dbc98f0d26c88d169e1b87b25b69
sunnykreative <sunnykr78.kreative@gmail.com>
2016-09-26 16:15:55 +0530

    New changes appended

--- djangoScrapper/extract/models.py
+++ djangoScrapper/extract/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Scrape(models.Model):	
-	import requests
-	r = requests.get("https://www.amazon.com/dp/b0042rum9g",headers={})
--- djangoScrapper/extract/views.py
+++ djangoScrapper/extract/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.template import RequestContext
-from .models import Scrape
 
 # Create your views here.
 def index(request):
 	return render(request,'home.html',{})
 
 def api(request):
-
-	return render(request,'api.html',{'request':request,'data':Scrape})
+	import requests
+	from bs4 import BeautifulSoup
+	r = requests.get(request.GET['url'],headers={'user-agent': 'Extract/0.0.1'})
+	soup = BeautifulSoup(r.content, 'html.parser')
+	images = soup.find_all('img')
+	
+	return render(request,'api.html',{'request':request,'images':images})
 
 def contact(request):
 	return render(request,'contact.html',{})	
