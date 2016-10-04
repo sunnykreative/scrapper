75b9a4e3228a571e134aeec392a57f8b01b297db
sunnykreative <sunnykr78.kreative@gmail.com>
2016-10-04 16:23:35 +0530

    Image and name scrapper extracted from the amazon

--- djangoScrapper/extract/views.py
+++ djangoScrapper/extract/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
+from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
@@ -14,7 +16,7 @@ def api(request):
 	name = soup.find_all("span", id="productTitle")
 	price = soup.find_all("span", id="priceblock_ourprice")
 	
-	return render(request,'api.html',{'request':request,'image':image,'name':name,'price':price})
+	return render(request,'api.html',{'request':request,'images':image,'name':name,'price':price})
 
 def contact(request):
 	return render(request,'contact.html',{})	
