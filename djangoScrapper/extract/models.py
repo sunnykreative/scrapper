5ab24d4d8f9d0181df81e23d87fc4a21209e43c1
apple <apple@apples-MacBook-Pro.local>
2016-11-29 18:18:04 +0530

    Traverse done for name

--- djangoScrapper/extract/models.py
+++ djangoScrapper/extract/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-import os
+import os,time
 from urllib.parse import urlparse
 
 # Create your models here.
@@ -31,15 +31,29 @@ def scrape_data(r,request):
 				# review=htmlContent.select(row[3]) if (row[3]!='') else ''
 				# productDescription=htmlContent.select(row[4]) if (row[4]!='') else ''
 
-
+		# If Domain is not mentioned on the dataset
 		if(domainExists!='true'):
-			name='TRAVERSE WILL BE DONE'
-			image='TRAVERSE WILL BE DONE'
-			price='TRAVERSE WILL BE DONE'
-			details='TRAVERSE WILL BE DONE'
-			productDescription='TRAVERSE WILL BE DONE'
-			review='TRAVERSE WILL BE DONE'
-			productDetailsTable='TRAVERSE WILL BE DONE'
+			csvFileNotExist=os.path.abspath(os.path.dirname(__file__))+'/../static/commondataset.csv'
+			with open(csvFileNotExist) as f:
+				readerCommon = csv.reader(f, delimiter=',')
+				commondomainExists='false'
+				for itemRow in readerCommon:
+					# 0 for image 1 for name 2 is price 3 is review 4 is description 5 is domain
+					if(itemRow[5]==domain):
+						commondomainExists = 'true';
+						image=htmlContent.select(itemRow[0]) if (itemRow[0]!='') else ''
+						name = htmlContent.select(itemRow[1]) if (itemRow[1]!='') else ''
+						price=htmlContent.select(itemRow[2]) if (itemRow[2]!='') else ''
+						review=htmlContent.select(itemRow[3]) if (itemRow[3]!='') else ''
+						productDescription=htmlContent.select(itemRow[4]) if (itemRow[4]!='') else ''
+
+			if(commondomainExists!='true'):
+				name=htmlContent.select('h1') if(htmlContent.select('h1')) else ''
+				
+				image=''
+				price=''
+				productDescription=''
+				review=''
 
 	return {'request':request,'images':image,'name':name,'price':price,'description':productDescription,'review':review}
 
