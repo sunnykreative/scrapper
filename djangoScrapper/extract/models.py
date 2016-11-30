19602f72d58185e77e22d18c67d302252ca2e1b0
apple <apple@apples-MacBook-Pro.local>
2016-11-30 15:49:01 +0530

    Final changes done. Bugs fixed

--- djangoScrapper/extract/models.py
+++ djangoScrapper/extract/models.py
@@ -33,32 +33,29 @@ def scrape_data(r,request):
 
 		# If Domain is not mentioned on the dataset
 		if(domainExists!='true'):
-			csvFileNotExist=os.path.abspath(os.path.dirname(__file__))+'/../static/commondataset.csv'
-			with open(csvFileNotExist) as f:
-				readerCommon = csv.reader(f, delimiter=',')
-				commondomainExists='false'
-				for itemRow in readerCommon:
-					# 0 for image 1 for name 2 is price 3 is review 4 is description 5 is domain
-					if(itemRow[5]==domain):
-						commondomainExists = 'true';
-						image=htmlContent.select(itemRow[0]) if (itemRow[0]!='') else ''
-						name = htmlContent.select(itemRow[1]) if (itemRow[1]!='') else ''
-						price=htmlContent.select(itemRow[2]) if (itemRow[2]!='') else ''
-						review=htmlContent.select(itemRow[3]) if (itemRow[3]!='') else ''
-						productDescription=htmlContent.select(itemRow[4]) if (itemRow[4]!='') else ''
+			name=htmlContent.select('h1') if(htmlContent.select('h1')) else ''
+			imageurl = htmlContent.find('meta', attrs={'property': 'og:image', 'content': True})
+			if imageurl:
+			    image = '<img class="maxWidth600" src="'+imageurl['content']+'"/>'
+			else:
+			    image='<img class="maxWidth600" src="/static/images/no-image.jpg"/>'
 
-			if(commondomainExists!='true'):
-				name=htmlContent.select('h1') if(htmlContent.select('h1')) else ''
-				imageurl = htmlContent.find('meta', attrs={'property': 'og:image', 'content': True})
-				if imageurl:
-				    image = '<img class="maxWidth600" src="'+imageurl['content']+'"/>'
-				else:
-				    image='<img class="maxWidth600" src="/static/images/no-image.jpg"/>'
-
-				# image=''
+			csvFileNotExist=os.path.abspath(os.path.dirname(__file__))+'/../static/commondataset.csv'			
+			with open(csvFileNotExist) as common:
+				readerCommon = csv.reader(common, delimiter=',')
 				price=''
-				productDescription=''
 				review=''
+				productDescription=''
+				for itemRow in readerCommon:
+					# 0 for price
+					try:
+					    priceTest=htmlContent.select(itemRow[0])
+					    if(priceTest):
+					    	price = priceTest
+					    
+					except NameError:
+					    Nochange = None
+
 
 	return {'request':request,'images':image,'name':name,'price':price,'description':productDescription,'review':review}
 
